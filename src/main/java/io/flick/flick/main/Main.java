49bb350ac920794a4bb8547dff14c4218ba110cb
Ybrin <koray@koska.at>
2017-01-20 20:19:00 +0100

    Initial commit

---
+++ src/main/java/io/flick/flick/main/Main.java
@@ -0,0 +1,11 @@
+package io.flick.flick.main;
+
+/**
+ * Created by Koray on 20.01.2017.
+ */
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println("TestExample");
+    }
+}
